95ef5021256a5eab1fed84248f2d805b31ab96b2
Sandeep Kumar Patra <spsandy101@gmail.com>
2017-02-06 23:10:17 -0500

    Update webScrapping.R

--- webScrapping.R
+++ webScrapping.R
@@ -43,30 +43,3 @@ for (dateindex in c(0:7)) {
         na.omit(paste("C:/Users/sparta/Desktop/R/Microsoft/greek/greeks_Table_",dateindex,".csv"))
     }
 }
-
-
-
-#####
-# Password protected website scraping
-#####
-
-handle <- handle("https://webauth.usf.edu") 
-path   <- "alogin?service=https%3A%2F%2Fusflearn.instructure.com%2Flogin%2Fcas"
-
-# fields found in the login form.
-login <- list(
-  canvas_userame = "UserId"
-  ,canvas_password  = "Password"
-  ,canvas_redirect_url = 
-    "https://usflearn.instructure.com/courses/1178127/gradebook"
-)
-
-response <- POST(handle = handle, path = path, body = login)
-
-
-#gradeTable= XML:: readHTMLTable(content(response))
-gradeTable= readHTMLTable(response, header = TRUE, which = 1, stringsAsFactors=FALSE)
-
-
-
-
